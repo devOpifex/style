1cb0ba08636eb88791be165d3ca9ddb14b40c722
John Coene <jcoenep@gmail.com>
2023-10-15 16:50:30 +0200

    feat: support file

--- R/core.R
+++ R/core.R
@@ -1,24 +1,22 @@
 #' Style
 #' 
-#' Generate CSS
+#' Generate CSS, set either `dir` or `file`.
 #' 
 #' @param dir Directory containing R files to scan.
+#' @param file File to scan.
 #' @param out Path to output CSS file.
 #' @param verbose Whether to warn on classes that fail to parse.
 #' 
 #' @export
-style <- function(dir, out, verbose = FALSE) {
-  if(missing(dir))
-    stop("missing `dir`")
-
-  if(missing(out))
-    stop("missing `out`")
+style <- function(dir = NULL, file = NULL, out = "style.min.css", verbose = FALSE) {
+  if(is.null(dir) && is.null(file))
+    stop("set `dir` or `file`")
 
   executable <- Sys.which("styler")
 
   if(executable == "")
     executable <- system.file("styler", package = "style")
 
-  args <- c(dir = dir, out = out, verbose = verbose)
+  args <- c(dir = dir, file = file, out = out, verbose = verbose)
   system2(executable, args)
 }
